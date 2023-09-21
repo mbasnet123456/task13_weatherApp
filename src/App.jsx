6b7cbd2ed908b2dc8c76cb3aca3d6c851c211c9c
manoj <mbasnet012@gmail.com>
2023-09-20 21:59:26 -0500

    sunny view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-
+import Sunny from "./component/sunny";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  return <div>Hello weather app</div>;
+  return (
+    <div>
+      Hello
+      <Sunny />
+    </div>
+  );
 }
 
 export default App;
