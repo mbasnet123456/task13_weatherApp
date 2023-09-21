694b1d8f6b89f1ed4d021522d390c2319132d107
Bikalp Timalsina <bikalp16@gmail.com>
2023-09-20 20:43:41 -0700

    re rendering components using state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { useState } from "react";
 import Sunny from "./component/sunny";
 import "./App.css";
 import RainyWeather from "./component/rainyWeather";
+import WeatherApp from "./component/weatherApp";
 
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
