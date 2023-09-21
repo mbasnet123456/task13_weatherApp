77ed5b8e8e3d8d22d2cf3586864af99d1529b3a2
Bikalp Timalsina <bikalp16@gmail.com>
2023-09-20 21:20:27 -0700

    updated few issues

--- src/component/rainyWeather.jsx
+++ src/component/rainyWeather.jsx
@@ -4,13 +4,12 @@ import styles from "../styles/rainyWeather.module.css";
 const RainyWeather = () => {
   return (
     <div className={styles.rainyContainer}>
-      <div
-        className={styles.rainyBackground}
-        style={{
-          backgroundImage:
-            "url('https://wallpaperaccess.com/full/9142345.gif')",
-        }}
-      ></div>
+      <div className={styles.rainyBackground}>
+        <img
+          className={styles.rain}
+          src="https://wallpapercave.com/wp/wp3146357.jpg"
+        />
+      </div>
 
       <div className={styles.rainyText}>Welcome to Rainy Weather</div>
     </div>
